c64cc7dd4de10deec3f15b0ba601fbe0b4297e8b
razor1895 <982959598@qq.com>
2017-03-11 01:17:56 +0800

    add tabbar and launch background image

---
+++ src/images/Icon.js
@@ -0,0 +1,19 @@
+const home = require('./home.png');
+const homeActive = require('./home_active');
+const reading = require('./reading.png');
+const readingActive = require('./reading_active');
+const music = require('./music.png');
+const musicActive = require('./music_active');
+const movie = require('./movie.png');
+const movieActive = require('./movie_active');
+
+export default {
+  home,
+  homeActive,
+  reading,
+  readingActive,
+  music,
+  musicActive,
+  movie,
+  movieActive
+};
