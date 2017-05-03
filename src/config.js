75f949df374f6faa1648758dac31fc88dda5540e
razor1895 <982959598@qq.com>
2017-05-03 21:27:35 +0800

    升级One api 版本

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export default {
   domain: 'http://v3.wufazhuce.com:8000',
   apiPath: '/api',
   imgUrl: 'http://image.wufazhuce.com/',
-  apiVersion: 'v4.1.1',
+  apiVersion: 'v4.2.0',
   dev: true,
 };
