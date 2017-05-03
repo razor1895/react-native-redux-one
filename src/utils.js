4047ffbed5ea6faa651d267a72099e8ca8f58fc3
razor1895 <982959598@qq.com>
2017-05-04 00:06:26 +0800

    日期格式化函数

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+export const formatDate = (source) => {
+  const date = new Date(source);
+  const dateTime = date.getTime();
+  const nowTime = new Date().getTime();
+  const hours = (nowTime - dateTime) / (1000 * 60 * 60);
+  const day = parseInt(hours / 24, 10);
+  let returnDateStr = '';
+
+  if (hours < 24) {
+    returnDateStr = `${parseInt(hours, 10)}小时前`;
+  } else if (day > 3) {
+    const year = date.getFullYear();
+    const month = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'June.', 'July.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'][date.getDay()];
+
+    returnDateStr = `${date.getDate()} ${month}${year}`;
+  } else {
+    returnDateStr = `${day}天前`;
+  }
+
+  return returnDateStr;
+};
