9512c0a0736b87d22b3785f326c42954d79d730f
razor1895 <982959598@qq.com>
2017-05-12 00:27:51 +0800

    Fix typo

--- src/epics/reading.js
+++ src/epics/reading.js
@@ -1,6 +1,6 @@
 import * as ActionTypes from '../ActionTypes';
 import * as readingServices from '../services/reading';
-import { receiveReadingFeedsList } from '../actions';
+import { receiveReadingFeedsList, receiveStory } from '../actions';
 
 export const fetchReadingFeedsListEpic = action$ =>
   action$.ofType(ActionTypes.REQUESTED_READING_FEEDS_LIST)
@@ -13,6 +13,6 @@ export const fetchStoryEpic = action$ =>
   action$.ofType(ActionTypes.REQUESTED_STORY)
     .switchMap(action =>
       readingServices.getStory(action.payload.storyId, action.payload.params)
-        .map(res => receiveReadingFeedsList(action.payload.storyId, res.data))
+        .map(res => receiveStory(action.payload.storyId, res.data))
     );
 
