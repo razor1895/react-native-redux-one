44e50ad16f17e9356d67c8b9e5fcf6989da4f6e6
razor1895 <982959598@qq.com>
2017-05-11 23:39:18 +0800

    获取文章接口

--- src/services/reading.js
+++ src/services/reading.js
@@ -1,3 +1,4 @@
 import * as requestService from './request';
 
 export const getReadingFeedsList = (startId, formData) => requestService.get(`/channel/reading/more/${startId}`, formData);
+export const getStory = (storyId, formData) => requestService.get(`/essay/${storyId}`, formData);
