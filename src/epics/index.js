ad41c212a69a97978f79394f6d8caf6dc459ba00
razor1895 <982959598@qq.com>
2017-05-11 23:44:57 +0800

    fetchStoryEpic

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,12 +1,13 @@
 import { combineEpics } from 'redux-observable';
 import searchUsers from './user';
-import { fetchReadingFeedsListEpic } from './reading';
+import { fetchReadingFeedsListEpic, fetchStoryEpic } from './reading';
 import { fetchMovieFeedsListEpic } from './movie';
 import { fetchMusicFeedsListEpic, fetchPlayableSongUrl } from './music';
 import { fetchHomeFeedsListEpic, fetchHomeIdListEpic } from './home';
 
 export default combineEpics(
   // searchUsers,
+  fetchStoryEpic,
   fetchReadingFeedsListEpic,
   fetchMovieFeedsListEpic,
   fetchMusicFeedsListEpic,
