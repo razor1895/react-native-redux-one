ed4779a02bd27b1f12f06fac3345090471244f80
razor1895 <982959598@qq.com>
2017-05-14 14:35:15 +0800

    receive strory reducer

--- src/reducers/reading.js
+++ src/reducers/reading.js
@@ -4,8 +4,14 @@ import * as ActionTypes from '../ActionTypes';
 
 const initialState = Immutable.fromJS({
   feedsList: [],
+  story: {}
 });
 
 export default createReducer(initialState, {
   [ActionTypes.RECEIVED_READING_FEEDS_LIST]: (state, action) => state.set('feedsList', Immutable.fromJS(action.payload.dataList)),
+  [ActionTypes.RECEIVED_STORY]: (state, action) => state.merge({
+    story: state.get('story').merge({
+      [action.payload.data.content_id]: action.payload.data
+    })
+  })
 });
