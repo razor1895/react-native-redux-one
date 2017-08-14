48159ded3eeaa0d16e9f5ca8cbe00f73083c97e6
razor1895 <982959598@qq.com>
2017-08-14 15:42:20 +0800

    update react-native version

--- src/reading/views/ReadingTabDetail.js
+++ src/reading/views/ReadingTabDetail.js
@@ -5,10 +5,9 @@ import {
   Text,
   Dimensions,
   ScrollView,
-  WebView
 } from 'react-native';
 import { connect } from 'react-redux';
-import WKWebView from 'react-native-wkwebview-reborn';
+import HTMLView from 'react-native-htmlview';
 import { requestStory } from '../../actions';
 
 const { width } = Dimensions.get('window');
@@ -27,19 +26,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     borderBottomColor: '#fff'
+  },
+  webViewText: {
+    fontSize: 16,
+    marginBottom: 20,
+    lineHeight: 32
+  },
+  strong: {
+    fontWeight: 'bold',
+  },
+  marginZero: {
+    marginBottom: 0,
   }
 });
 
 class ReadingDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      injectJS: `var bodyStyle = window.document.getElementsByTagName("body")[0].style;
-      bodyStyle.padding = "0 20px";
-      bodyStyle.marginTop = "60px";
-      bodyStyle.marginBottom = "600px";
-      alert(1)`
-    };
   }
 
   componentDidMount() {
@@ -49,6 +52,18 @@ class ReadingDetail extends Component {
     this.props.requestStory(storyId, { source, source_id });
   }
 
+  renderNode(node, index, siblings, parent, defaultRenderer) {
+    if (node.name === 'p') {
+      return (
+        <Text style={styles.webViewText}>{node.children.map(v => v.data)}</Text>
+      )
+    }
+
+    if (node.type === 'text') {
+      return;
+    }
+  }
+
   render() {
     const data = this.props.storys.get(this.storyId);
 
@@ -56,21 +71,39 @@ class ReadingDetail extends Component {
       return null;
     }
 
-    const html_content = data.get('hp_content');
+    const htmlContent = `
+    <div>
+      <h1>Some really snazzy html</h1>
+      <p>Bacon ipsum dolor amet turducken shank ribeye rump. T-bone boudin chicken, alcatra pig ribeye turkey venison ground round rump sirloin landjaeger spare ribs. Ball tip fatback prosciutto, salami swine kielbasa ground round andouille rump pig tongue sirloin cow beef ribs jerky. Cupim kielbasa short loin sausage pork landjaeger andouille jerky turkey capicola picanha beef.</p>
+      <h5 style="margin-bottom:0px;">Unsupported</h5>
+      <ol>
+        <li>​<span style="background-color: rgb(192, 80, 77);"><span style="color: rgb(255, 255, 0);"><strong>Blink</strong></span></span> <span style="background-color: rgb(255, 255, 0);"><span style="color: rgb(192, 80, 77);"><strong>tag</strong></span></span></li>
+        <li><span>rolling Marquee... Scr</span></li>
+      </ol>
+      <h5 style="margin-bottom:0px;">Supported</h5>
+      <ul>
+        <li><span>​<strong>The</strong> <em>kitchen</em> <u>sink</u></span></li>
+        <li><span style="font-size: 20px;">The</span> <span style="font-size: 11px;">bathroom</span> <strong><span style="color: rgb(49, 133, 155);">sink</span></strong></li>
+      </ul>
+      <p style="text-align: right;"><span style="color: rgb(118, 146, 60);"><strong>The sink that lives over here</strong></span></p>
+      <p style="text-align: center;"><span style="color: rgb(227, 108, 9);">And other HTML-y goodness :-)</span></p>
+      <p style="text-align: center;">
+        <img src="https://facebook.github.io/react-native/img/opengraph.png" style="width:100px; height:100px;"/>
+      </p>
+      <p>Drumstick strip steak ball tip pancetta jerky kevin filet mignon tri-tip. Ground round filet mignon meatloaf salami rump kielbasa fatback beef picanha tail tongue ball tip pork loin ham hock strip steak. Swine short ribs cow kielbasa ribeye tri-tip pancetta porchetta beef ribs fatback shoulder. Filet mignon pork loin salami brisket, cupim shoulder turkey prosciutto tenderloin beef ribs. Tail leberkas brisket t-bone beef.</p>
+    </div>
+    `;
 
     return (
-      <View
+      <ScrollView
         style={styles.container}
       >
-        <WKWebView
-          source={{ html: html_content }}
+        <HTMLView
+          value={htmlContent}
           style={styles.webView}
-          injectedJavaScript={this.state.injectJS}
-        >
-          <Text style={styles.title}>{data.get('hp_title')}</Text>
-          <Text style={styles.author}>文 / {data.get('hp_author')}</Text>
-        </WKWebView>
-      </View>
+          renderNode={this.renderNode}
+        />
+      </ScrollView>
     );
   }
 }
