99053fa7ee3982d274e32bf6d7d68b9aaa83bd7c
razor1895 <982959598@qq.com>
2017-08-17 15:11:12 +0800

    Me

---
+++ src/MeScreen/views/Me.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default () => (
+  <View style={styles.container}>
+    <Text>
+      All Feeds
+    </Text>
+  </View>
+);
