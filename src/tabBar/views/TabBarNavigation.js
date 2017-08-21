b43c411d9993726d7b08ef8fdb23c1bed61f0e19
razor1895 <982959598@qq.com>
2017-08-22 01:30:16 +0800

    custome status bar color on both Android and iOS platforms

--- src/tabBar/views/TabBarNavigation.js
+++ src/tabBar/views/TabBarNavigation.js
@@ -1,15 +1,22 @@
 // React
 import React from 'react';
+import { View } from 'react-native';
 //Redux
 import { connect } from 'react-redux';
 // Navigation
 import { addNavigationHelpers } from 'react-navigation';
 import TabBar from '../navigationConfiguration';
+import StatusBarEncapsulation from '../../components/StatusBarEncapsulation';
+
+const containerStyle = { flex: 1 };
 
 const TabBarNavigation = (props) => {
   // const { dispatch, navigationState } = props;
   return (
-    <TabBar />
+    <View style={containerStyle}>
+      <StatusBarEncapsulation backgroundColor="#5E8D48" barStyle="light-content" hidden />
+      <TabBar />
+    </View>
   );
 };
 
