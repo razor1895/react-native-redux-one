035a928fa901569cb04468d4593bb582b04cc088
razor1895 <982959598@qq.com>
2017-08-22 15:37:32 +0800

    update RadioCard UI

--- src/components/RadioCard.js
+++ src/components/RadioCard.js
@@ -7,6 +7,7 @@ import {
   Image,
   TouchableOpacity
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import { HEART, HEART_FILL, SHARE } from '../images';
 
@@ -18,16 +19,34 @@ const styles = StyleSheet.create({
     width,
     position: 'relative'
   },
+  radioLayer: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width,
+    height: 240,
+    zIndex: 10,
+  },
   background: {
     position: 'absolute',
     left: 0,
     top: 0,
     width,
     height: 240,
-    resizeMode: 'cover'
+    resizeMode: 'cover',
   },
   fmInfo: {
     paddingLeft: 20,
+  },
+  titleInfo: {
+    position: 'absolute',
+    bottom: 58,
+    width,
+    alignItems: 'center'
+  },
+  smallTitle: {
+    fontSize: 12,
+    color: '#a7a7a7'
   },
   volume: {
     backgroundColor: 'transparent',
@@ -39,9 +58,8 @@ const styles = StyleSheet.create({
   },
   cardFooter: {
     borderTopWidth: StyleSheet.hairlineWidth,
-    borderTopColor: '#eee',
+    borderTopColor: '#666',
     height: 40,
-    backgroundColor: 'rgba(0,0,0,.3)',
     width,
     position: 'absolute',
     bottom: 0,
@@ -57,7 +75,8 @@ const styles = StyleSheet.create({
     position: 'relative',
     top: 8,
     marginLeft: 2,
-    width: 30
+    width: 30,
+    backgroundColor: 'transparent'
   },
   heart: {
     width: 16,
@@ -106,10 +125,21 @@ export default class RadioCard extends Component {
 
     return (
       <View style={styles.container}>
+        {data.get('volume')
+          ? (
+            <LinearGradient
+              start={{ x: 0.0, y: 0.0 }}
+              locations={[0.0, 1.0]}
+              end={{ x: 0.0, y: 1.0 }}
+              colors={['rgba(0,0,0,.2)', 'rgba(0,0,0,.5)']}
+              style={styles.radioLayer}
+            />
+          ) : null
+        }
         <Image style={styles.background} source={{ uri: data.get('img_url') }} />
-        <View style={styles.fmInfo}>
+        <View style={data.get('volume') ? styles.fmInfo : styles.titleInfo}>
           <Text style={styles.volume}>{data.get('volume')}</Text>
-          <Text style={styles.title}>{data.get('title')}</Text>
+          <Text style={[styles.title, !data.get('volume') && styles.smallTitle]}>{data.get('title')}</Text>
         </View>
         <View style={styles.cardFooter}>
           <View style={styles.authorInfo}>
