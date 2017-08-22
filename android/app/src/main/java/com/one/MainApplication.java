2ff66d3b21f110d487e7acc1a4373d96f3dfb075
razor1895 <982959598@qq.com>
2017-08-22 15:36:14 +0800

    react-native-linear-gradient

--- android/app/src/main/java/com/one/MainApplication.java
+++ android/app/src/main/java/com/one/MainApplication.java
@@ -3,6 +3,7 @@ package com.one;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.tanguyantoine.react.MusicControl;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNSoundPackage(),
             new MusicControl()
